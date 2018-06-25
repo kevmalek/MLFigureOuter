0c53b1a076821cd6bf366238f00e4d0f44d9da64
Kevin Malek <kevinmalek@kevins-macbook-pro.local>
2018-06-25 15:47:36 -0700

    Initial Commit

---
+++ MLFigureOuter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MLFigureOuter
+//
+//  Created by Kevin Malek on 6/25/18.
+//  Copyright © 2018 The RealReal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
